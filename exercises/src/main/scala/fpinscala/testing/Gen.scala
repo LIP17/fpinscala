24c253db260d7591e3446a1efce08db683ade6d4
Li Pei <liopei19nn@gmail.com>
2017-04-27 00:19:07 -0400

    chapter8: wip

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -14,20 +14,38 @@ shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Prop {
+  // ex3: given "def check: Boolean", return this
+  def &&(p: Prop): Prop = new Prop {
+    def check = this.check && p.check
+  }
+  def check: Boolean
+
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
 }
 
 object Prop {
+  type SuccessCount = Int
+  type FailedCase = String
+
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
 object Gen {
   def unit[A](a: => A): Gen[A] = ???
+
+  // ex4: implement choose to pick a random number between start and stopExclusive
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive-start)))
+
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
-}
+// cannot exist with Gen(sample: State[RNG, A])
+//trait Gen[A] {
+//  def map[A,B](f: A => B): Gen[B] = ???
+//  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+//}
+
+case class Gen[+A](sample: State[RNG, A])
 
 trait SGen[+A] {
 
