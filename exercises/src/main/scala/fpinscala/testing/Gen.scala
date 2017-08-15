df8367f60b7e258ff9d59943432d6e744d5c7417
LIP17 <lipei.work@gmail.com>
2017-08-15 00:38:33 -0400

    wip

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -14,9 +14,24 @@ shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Prop {
+// Ex 3 implement && with `def check: Boolean`
+  /* Use Prop.this to reference variable out of scope
+  *  https://stackoverflow.com/questions/8687010/accessing-a-value-from-the-outer-scope-when-a-local-member-with-the-same-name-is
+  * */
+//  def &&(p: Prop): Prop = new Prop {
+//    override def check: Boolean = {
+//      Prop.this.check && p.check
+//    }
+//  }
+//  def check: Boolean
+
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
 }
 
 object Prop {
+
+  type SuccessCount = Int
+  type FailedCase = String
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
